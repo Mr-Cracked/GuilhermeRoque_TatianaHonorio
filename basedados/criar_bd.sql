49ed553f9815f875388bc09bfd245cf2c025b923
Mr-Cracked <guiroque46@gmail.com>
2024-04-25 17:02:27 +0100

    Update criar_bd.sql

--- basedados/criar_bd.sql
+++ basedados/criar_bd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2024 at 01:44 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 25-Abr-2024 às 18:01
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gestaocursos`
+-- Banco de dados: `gestaocursos`
 --
 CREATE DATABASE IF NOT EXISTS `gestaocursos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `gestaocursos`;
@@ -26,7 +26,7 @@ USE `gestaocursos`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `curso`
+-- Estrutura da tabela `curso`
 --
 
 CREATE TABLE `curso` (
@@ -34,34 +34,56 @@ CREATE TABLE `curso` (
   `nome` varchar(100) NOT NULL,
   `descricao` varchar(100) NOT NULL,
   `vagas` int(40) NOT NULL,
+  `vagas_preenchidas` int(40) NOT NULL,
   `data_inicio` date NOT NULL,
-  `data_fim` date NOT NULL
+  `data_fim` date NOT NULL,
+  `metodo_selecao` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `curso`
+-- Extraindo dados da tabela `curso`
 --
 
-INSERT INTO `curso` (`id_curso`, `nome`, `descricao`, `vagas`, `data_inicio`, `data_fim`) VALUES
-(1, 'Engenharia Informática', 'Enegenharia para a informática', 150, '2024-03-05', '2025-04-09'),
-(2, 'Engenhaira do desemprego', 'Em outras palavras artes', 200, '2024-04-02', '2024-10-04');
+INSERT INTO `curso` (`id_curso`, `nome`, `descricao`, `vagas`, `vagas_preenchidas`, `data_inicio`, `data_fim`, `metodo_selecao`) VALUES
+(1, 'Engenharia Informática', 'Engenharia para a informática', 150, 0, '2024-03-05', '2025-04-09', ''),
+(2, 'Engenhaira do desemprego', 'Em outras palavras artes', 200, 0, '2024-04-02', '2024-10-04', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inscricao`
+-- Estrutura da tabela `inscricao`
 --
 
 CREATE TABLE `inscricao` (
   `nome` varchar(60) NOT NULL,
-  `id_curso` int(40) NOT NULL,
-  `descricao` varchar(300) NOT NULL
+  `id_curso` int(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilizador`
+-- Estrutura da tabela `lecionar`
+--
+
+CREATE TABLE `lecionar` (
+  `nome` varchar(100) NOT NULL,
+  `id_curso` int(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `selecao`
+--
+
+CREATE TABLE `selecao` (
+  `metodo_selecao` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `utilizador`
 --
 
 CREATE TABLE `utilizador` (
@@ -74,61 +96,82 @@ CREATE TABLE `utilizador` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `utilizador`
+-- Extraindo dados da tabela `utilizador`
 --
 
 INSERT INTO `utilizador` (`nome`, `password`, `morada`, `email`, `telemovel`, `tipo_utilizador`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3', 'fado123', 'fasdfa@gmail.com', 2321421, 3),
+('admin', '21232f297a57a5a743894a0e4a801fc3', '', '', 0, 3),
 ('aluno', 'ca0cd09a12abade3bf0777574d9f987f', 'aluno', 'aluno@gmail.com', 123456789, 1),
 ('docente', 'ac99fecf6fcb8c25d18788d14a5384ee', 'fadc', 'fasdfa@gmail.com', 2321421, 2),
 ('docente1', '085a4315b710361139bef0018d90ac48', 'fadinhoyauuuuuu', 'fasdfa@gmail.com', 2321421, 2),
 ('dsa', 'c20ad4d76fe97759aa27a0c99bff6710', 'sac', 'dsa@cfads.com', 2315, 0),
 ('edbhasg', '1558a650b39e1ffa6b715449f44dd475', 'gasgsgg', 'yeet@gmail.com', 225151, 0),
-('feas', '01cfcd4f6b8770febfb40cb906715822', 'vycdavhjac', 'hvcda@hbea.com', 425164789, 0);
+('feas', '01cfcd4f6b8770febfb40cb906715822', 'vycdavhjac', 'hvcda@hbea.com', 425164789, 0),
+('Guilherme', 'd6ee4e61af68a10f9bb9ee130313881f', 'rua da praca', 'guilherme.roque@ipcbcampus.pt', 973023847, 0);
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `curso`
+-- Índices para tabela `curso`
 --
 ALTER TABLE `curso`
   ADD PRIMARY KEY (`id_curso`);
 
 --
--- Indexes for table `inscricao`
+-- Índices para tabela `inscricao`
 --
 ALTER TABLE `inscricao`
   ADD PRIMARY KEY (`nome`,`id_curso`),
   ADD KEY `id_cursoFK` (`id_curso`);
 
 --
--- Indexes for table `utilizador`
+-- Índices para tabela `lecionar`
+--
+ALTER TABLE `lecionar`
+  ADD PRIMARY KEY (`nome`,`id_curso`),
+  ADD KEY `FK_id_curso` (`id_curso`);
+
+--
+-- Índices para tabela `selecao`
+--
+ALTER TABLE `selecao`
+  ADD PRIMARY KEY (`metodo_selecao`);
+
+--
+-- Índices para tabela `utilizador`
 --
 ALTER TABLE `utilizador`
   ADD PRIMARY KEY (`nome`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `curso`
+-- AUTO_INCREMENT de tabela `curso`
 --
 ALTER TABLE `curso`
   MODIFY `id_curso` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Constraints for dumped tables
+-- Restrições para despejos de tabelas
 --
 
 --
--- Constraints for table `inscricao`
+-- Limitadores para a tabela `inscricao`
 --
 ALTER TABLE `inscricao`
   ADD CONSTRAINT `id_cursoFK` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `nome_FK` FOREIGN KEY (`nome`) REFERENCES `utilizador` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `lecionar`
+--
+ALTER TABLE `lecionar`
+  ADD CONSTRAINT `FK_id_curso` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_nome` FOREIGN KEY (`nome`) REFERENCES `utilizador` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
