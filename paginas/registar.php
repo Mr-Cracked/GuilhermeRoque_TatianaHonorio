bee716caf37acc9deb76d7d37620ebd46ad753c7
Mr-Cracked <guiroque46@gmail.com>
2024-05-19 14:23:27 +0100

    Update registar.php

--- paginas/registar.php
+++ paginas/registar.php
@@ -2,7 +2,7 @@
 include '../basedados/basedados.h';
 session_start();
 
-if(empty($_SESSION['tipo_utilizador'] || $_SESSION['tipo_utilizador'] == 3)){
+if(empty($_SESSION['tipo_utilizador']) || $_SESSION['tipo_utilizador'] == 3){
     
 
     if (empty($_POST['nivelacesso'])) {
@@ -10,69 +10,79 @@ if(empty($_SESSION['tipo_utilizador'] || $_SESSION['tipo_utilizador'] == 3)){
     } else {
         $nivel = $_POST['nivelacesso'];
     }
-    $nome = $_POST['username'];
-    $password = $_POST['password']; // A senha ainda não está encriptada
-    $email = $_POST['email'];
-    $telemovel = $_POST['telemovel'];
-    $morada = $_POST['morada'];
 
-    // Encriptar a senha
-    $senha_encriptada = md5($password);
-
-    
-    $sql = "SELECT * FROM utilizador WHERE nome='$nome'";
-    $result = mysqli_query($conn, $sql);
-
-    
-    if (mysqli_num_rows($result) > 0) {
-        if ($_SESSION['tipo_utilizador'] == 3) {
-            echo '<link rel="stylesheet" href="bootstrap.css">';
-            echo '<div class="alert alert-dismissible alert-danger">';
-            echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-            echo '<strong>Erro!</strong> <a href="adicionarUtilizadorformulario.php" class="alert-link">Utilizador já existe</a></div>';
-        } else {
-            echo '<link rel="stylesheet" href="bootstrap.css">';
-            echo '<div class="alert alert-dismissible alert-danger">';
-            echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-            echo '<strong>Erro!</strong> <a href="registarformulario.php" class="alert-link">Utilizador já existe</a></div>';
-        }
-    } else {
-        if (empty($nivel)) {
-            
-            $sql = "INSERT INTO utilizador (nome, password, email, telemovel, morada) VALUES ('$nome', '$senha_encriptada', '$email', '$telemovel', '$morada')";
-            $result = mysqli_query($conn, $sql);
-
-            
-            if ($result) {
-                echo '<link rel="stylesheet" href="bootstrap.css">';
-                echo '<div class="alert alert-dismissible alert-success">';
+    if(empty($_POST['username'])){
+        echo '<link rel="stylesheet" href="bootstrap.css">';
+                echo '<div class="alert alert-dismissible alert-danger">';
                 echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-                echo '<strong>Alerta!</strong> <a href="index.php" class="alert-link">Sucesso!!!</a></div>';
+                echo '<strong>Erro!</strong> <a href="index.php" class="alert-link">Não há dados suficientes</a></div>';
+    }else{
+        $nome = $_POST['username'];
+        $password = $_POST['password']; // A senha ainda não está encriptada
+        $email = $_POST['email'];
+        $telemovel = $_POST['telemovel'];
+        $morada = $_POST['morada'];
+
+        // Encriptar a senha
+        $senha_encriptada = md5($password);
+
+        
+        $sql = "SELECT * FROM utilizador WHERE nome='$nome'";
+        $result = mysqli_query($conn, $sql);
+
+        
+        if (mysqli_num_rows($result) > 0) {
+            if ($_SESSION['tipo_utilizador'] == 3) {
+                echo '<link rel="stylesheet" href="bootstrap.css">';
+                echo '<div class="alert alert-dismissible alert-danger">';
+                echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+                echo '<strong>Erro!</strong> <a href="adicionarUtilizadorformulario.php" class="alert-link">Utilizador já existe</a></div>';
             } else {
                 echo '<link rel="stylesheet" href="bootstrap.css">';
                 echo '<div class="alert alert-dismissible alert-danger">';
                 echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-                echo '<strong>Alerta!</strong> <a href="registarformulario.php" class="alert-link">ERRO!</a></div>';
+                echo '<strong>Erro!</strong> <a href="registarformulario.php" class="alert-link">Utilizador já existe</a></div>';
             }
         } else {
-            
-            $sql = "INSERT INTO utilizador (tipo_utilizador,nome, password, email, telemovel, morada) VALUES ('$nivel','$nome', '$senha_encriptada', '$email', '$telemovel', '$morada')";
-            $result = mysqli_query($conn, $sql);
+            if (empty($nivel)) {
+                
+                $sql = "INSERT INTO utilizador (nome, password, email, telemovel, morada) VALUES ('$nome', '$senha_encriptada', '$email', '$telemovel', '$morada')";
+                $result = mysqli_query($conn, $sql);
 
-            
-            if ($result) {
-                echo '<link rel="stylesheet" href="bootstrap.css">';
-                echo '<div class="alert alert-dismissible alert-success">';
-                echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-                echo '<strong>Alerta!</strong> <a href="utilizadores.php" class="alert-link">Sucesso!!!</a></div>';
+                
+                if ($result) {
+                    echo '<link rel="stylesheet" href="bootstrap.css">';
+                    echo '<div class="alert alert-dismissible alert-success">';
+                    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+                    echo '<strong>Alerta!</strong> <a href="index.php" class="alert-link">Sucesso!!!</a></div>';
+                } else {
+                    echo '<link rel="stylesheet" href="bootstrap.css">';
+                    echo '<div class="alert alert-dismissible alert-danger">';
+                    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+                    echo '<strong>Alerta!</strong> <a href="registarformulario.php" class="alert-link">ERRO!</a></div>';
+                }
             } else {
-                echo '<link rel="stylesheet" href="bootstrap.css">';
-                echo '<div class="alert alert-dismissible alert-danger">';
-                echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
-                echo '<strong>Alerta!</strong> <a href="registarformulario.php" class="alert-link">ERRO!</a></div>';
+                
+                $sql = "INSERT INTO utilizador (tipo_utilizador,nome, password, email, telemovel, morada) VALUES ('$nivel','$nome', '$senha_encriptada', '$email', '$telemovel', '$morada')";
+                $result = mysqli_query($conn, $sql);
+
+                
+                if ($result) {
+                    echo '<link rel="stylesheet" href="bootstrap.css">';
+                    echo '<div class="alert alert-dismissible alert-success">';
+                    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+                    echo '<strong>Alerta!</strong> <a href="utilizadores.php" class="alert-link">Sucesso!!!</a></div>';
+                } else {
+                    echo '<link rel="stylesheet" href="bootstrap.css">';
+                    echo '<div class="alert alert-dismissible alert-danger">';
+                    echo '<button type="button" class="btn-close" data-bs-dismiss="alert"></button>';
+                    echo '<strong>Alerta!</strong> <a href="registarformulario.php" class="alert-link">ERRO!</a></div>';
+                }
             }
         }
     }
+
+    
 }else{
     header("Erro.jsp");
 }
